ad2fad6b292993030d62bb90a09e350cb7f99b31
Lars Bogner <lars.bogner@outlook.de>
2019-07-31 19:36:22 +0200

    error handling (issue n#9)

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import subject
 import user_IO
 
 subjects = []
-while True:
-    user_input = user_IO.user_input()
-    if user_input[0] == "input":
-        if user_input[1] == "grade":
-            subject_edit = subject.get_subject(subjects, user_input[2])
-            if subject_edit:
-                subject_edit.add_grade(user_input[3], subject.Grade(user_input[4], user_input[5]))
+try:
+    while True:
+        user_input = user_IO.user_input()
+        if user_input[0] == "input":
+            if user_input[1] == "grade":
+                subject_edit = subject.get_subject(subjects, user_input[2])
+                if subject_edit:
+                    subject_edit.add_grade(user_input[3], subject.Grade(user_input[4], user_input[5]))
+            elif user_input[1] == "category":
+                subject_edit = subject.get_subject(subjects, user_input[2])
+                if subject_edit:
+                    subject_edit.add_grade_category(user_input[3], user_input[4])
+            elif user_input[1] == "subject":
+                subjects.append(subject.Subject(user_input[2], _, _))
+    raise SystemError
+
+except Exception as e:
+    print('ERROR' + str(e))
